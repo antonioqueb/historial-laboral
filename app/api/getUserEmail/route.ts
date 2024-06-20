28cd4a13f1a798de77e1c837847bc196a2182bda
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-20 16:00:59 -0600

    Up

---
+++ app/api/getUserEmail/route.ts
@@ -0,0 +1,24 @@
+// app/api/getUserEmail/route.ts
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/authOptions";
+import { NextResponse } from "next/server";
+
+
+// Define the type for the session with the user email
+interface ExtendedSession {
+  user: {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  };
+}
+
+
+export async function GET() {
+    const session = await getServerSession(authOptions) as ExtendedSession | null;
+    if (!session || !session.user || !session.user.email) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    return NextResponse.json({ email: session.user.email }, { status: 200 });
+}
