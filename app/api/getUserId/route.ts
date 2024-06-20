4766545ce7e77742ab6628a62b506a2751f5d048
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-20 15:51:53 -0600

    Up

---
+++ app/api/getUserId/route.ts
@@ -0,0 +1,12 @@
+// app/api/getUserId/route.ts
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    return NextResponse.json({ id: session.user.id }, { status: 200 });
+}
