f0e91aa1992e402c6fc0bfea6a53d0fd08465029
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-20 10:37:20 -0600

    chore: Remove unused code and refactor form submission in DashboardEmployedAdmin component

--- components/component/dashboard-employed-admin.jsx
+++ components/component/dashboard-employed-admin.jsx
@@ -1,47 +1,20 @@
 // components\component\dashboard-employed-admin.jsx
 'use client';
 
-import { useState } from 'react';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
-import { createEmployee } from '@/app/actions/createEmployee';
-import { useSession } from 'next-auth/react';
 
 export default function DashboardEmployedAdmin() {
-  const { data: session } = useSession();
-  const userId = session?.user?.id;
-  const [error, setError] = useState(null);
-  const [success, setSuccess] = useState(null);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setError(null);
-    setSuccess(null);
-
-    const formData = new FormData(event.target);
-
-    try {
-      const result = await createEmployee(formData, userId);
-      if (result.success) {
-        setSuccess('Empleado agregado exitosamente');
-        event.target.reset();
-      } else {
-        setError(result.message + (result.error ? `: ${result.error}` : ''));
-      }
-    } catch (err) {
-      setError('Error al agregar el empleado');
-    }
-  };
 
   return (
     <div className="w-full mx-auto px-4 md:px-6 py-12">
       <div className="flex flex-col md:flex-row items-center justify-between mb-6">
         <h1 className="text-2xl font-bold mb-4 md:mb-0">Administrar Empleados</h1>
       </div>
-      <form onSubmit={handleSubmit}>
+      <form >
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-1 md:grid-cols-4 items-center gap-4">
             <Label className="text-right md:text-left md:col-span-1" htmlFor="name">
