c536c2d2b35cae66c9adfcd83af7ecad6d6e0933
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-20 22:53:18 -0600

    Actualizar etiquetas de formulario en la creación de empresas

--- components/component/dashboard-company-admin.tsx
+++ components/component/dashboard-company-admin.tsx
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from "react";
 import { useSession, signIn } from "next-auth/react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
 export default function CreateCompany() {
