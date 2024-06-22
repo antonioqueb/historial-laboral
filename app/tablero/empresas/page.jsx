90780c9d9c32cbc1f3675c93defc9b3c2f89456f
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-21 20:45:08 -0600

    Actualizar importación de componente en el archivo page.jsx

--- app/tablero/empresas/page.jsx
+++ app/tablero/empresas/page.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import DashboardEmployed from '@/components/component/dashboard-employed'
+import DashboardEmployed from '@/components/component/dashboard-employed-summary'
 
 const Empleados = () => {
   return (
