4867689bf5a6d8083a14ae54f88d304350801c16
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-22 13:45:41 -0600

    Up

---
+++ app/api/(companies)/getCompanyRFC/route.ts
@@ -0,0 +1,41 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../(auth)/auth/[...nextauth]/authOptions";
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+// Define the type for the session with the user ID
+interface ExtendedSession {
+  user: {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  };
+}
+
+const prisma = new PrismaClient();
+
+export async function GET(req: Request) {
+    const session = await getServerSession(authOptions) as ExtendedSession | null;
+    if (!session || !session.user || !session.user.id) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const userId = session.user.id;
+
+    try {
+        // Obtener las empresas del usuario
+        const companies = await prisma.company.findMany({
+            where: { userId: userId },
+            select: { rfc: true } // Seleccionar solo el campo RFC
+        });
+
+        // Extraer solo los RFCs de las empresas
+        const rfcs = companies.map(company => company.rfc);
+
+        return NextResponse.json({ rfcs }, { status: 200 });
+    } catch (error) {
+        // Convertir 'error' a tipo 'any'
+        return NextResponse.json({ error: (error as any).message }, { status: 500 });
+    }
+}
