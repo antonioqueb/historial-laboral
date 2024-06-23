45dd3ebf71d28482e14b4982cd9be7a22512d031
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-23 09:24:00 -0600

    Up

---
+++ app/api/(employeed)/listEmployeesByCompanyRFC/route.ts
@@ -0,0 +1,56 @@
+// app\api\(employeed)\listEmployeesByCompanyRFC\route.ts
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/(auth)/auth/[...nextauth]/authOptions";
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+// Define the type for the session with the user ID
+interface ExtendedSession {
+  user: {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  };
+}
+
+const prisma = new PrismaClient();
+
+export async function GET(req: Request) {
+    const session = await getServerSession(authOptions) as ExtendedSession | null;
+    if (!session || !session.user || !session.user.id) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const url = new URL(req.url);
+    const companyRFC = url.searchParams.get("rfc");
+
+    if (!companyRFC) {
+        return NextResponse.json({ error: "RFC is required" }, { status: 400 });
+    }
+
+    try {
+        // Obtener los empleados de la empresa por RFC
+        const employees = await prisma.employee.findMany({
+            where: {
+                company: {
+                    rfc: companyRFC
+                }
+            },
+            select: {
+                id: true,
+                name: true,
+                role: true,
+                department: true,
+                email: true,
+                phoneNumber: true,
+                // Añadir más campos si es necesario
+            }
+        });
+
+        return NextResponse.json({ employees }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: (error as any).message }, { status: 500 });
+    }
+}
