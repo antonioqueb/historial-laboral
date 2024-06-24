858ad39c853ac3da689e3a34f0152dfa8c464631
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-24 03:05:55 -0600

    feat: Configurar carga perezosa de imágenes remotas en el componente de barra lateral del tablero

--- components/component/dasboard-sidebar.jsx
+++ components/component/dasboard-sidebar.jsx
@@ -21,7 +21,7 @@ export default function DashboardSidebar() {
   return (
     <div className={`fixed inset-y-0 left-0 z-50 flex flex-col h-screen max-w-[280px] bg-white dark:bg-zinc-900 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:relative md:translate-x-0`}>
       <div className="flex items-center justify-between h-16 px-4 border-b border-zinc-200 dark:border-zinc-800">
-        <Link className="flex items-center gap-2 font-semibold text-lg md:text-xl border border-black" href="/">
+        <Link className="ml-4 flex items-center gap-2 font-semibold text-lg md:text-xl border border-black" href="/">
           <MountainIcon className="h-8 w-8 md:h-10 md:w-10" />
           {/* <span className="sr-only">Acme Inc</span> */}
         </Link>
