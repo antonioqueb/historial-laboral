77f6a57b5f7a33d9b2abfd512462c32f3e83fc3d
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-23 21:37:14 -0600

    feat: Configurar carga perezosa de imágenes remotas

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: '192.168.1.69',
+                port: '3008',
+                pathname: '/uploads/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
