ccd34664c8ee161247079ff8c4d06c429e4a3bc0
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-28 17:27:56 -0600

    Actualizar diseño de cuadrícula en el componente ResponseSearch

--- components/ResponseSearch.jsx
+++ components/ResponseSearch.jsx
@@ -15,18 +15,19 @@ export default function ResponseSearch({ reviews }) {
 
   return (
     <TooltipProvider>
-      <div className="w-full mt-6"> {/* Añadir margen superior */}
+      <div className="w-full mt-6">
+        {/* Añadir margen superior */}
         <section className="p-4 w-full bg-white dark:bg-zinc-800 shadow-lg rounded-lg">
           <div className="flex flex-wrap gap-6">
-            {reviews.map(({ id, title, description, rating, positive, documentation, company, createdAt }) => (
-              <Card key={id} className="flex w-full hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors duration-200">
+            {reviews.map(({ id, title, description, rating, positive, company, createdAt }) => (
+              <Card key={id} className="flex w-full flex-col hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors duration-200">
                 <div className="flex w-full">
                   <div className="flex flex-col justify-between w-full p-4">
-                    <CardHeader className="flex items-center justify-between p-0">
-                      <CardTitle className="text-xl font-semibold">
+                    <CardHeader className="flex flex-col md:flex-row items-start md:items-center justify-between p-0">
+                      <CardTitle className="text-lg font-semibold md:text-xl">
                         {title}
                       </CardTitle>
-                      <div className="flex items-center">
+                      <div className="flex items-center mt-2 md:mt-0">
                         {[...Array(rating)].map((_, index) => (
                           <AiFillStar key={index} className="text-yellow-500" />
                         ))}
@@ -46,7 +47,6 @@ export default function ResponseSearch({ reviews }) {
                         </Tooltip>
                         <span className="text-sm">Positiva: {positive ? "Sí" : "No"}</span>
                       </div>
-                      <p className="text-sm mt-2">Documentación: {documentation}</p>
                       <p className="text-sm mt-2">Empresa: {company.name}</p>
                       <p className="text-sm mt-2">Fecha: {new Date(createdAt).toLocaleDateString()}</p>
                     </CardContent>
