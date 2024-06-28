2cf068290d57b824a3dc1ce561323897815a6853
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-28 16:19:37 -0600

    chore: Remove unnecessary whitespace in SearchHome component

--- components/SearchHome.jsx
+++ components/SearchHome.jsx
@@ -59,6 +59,7 @@ export default function Component() {
             {error && <p className="text-red-500 mt-2">{error}</p>}
           </div>
           <ResponseSearch reviews={reviews} />
+          
         </div>
       </div>
     </main>
