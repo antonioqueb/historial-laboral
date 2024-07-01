9f29d2099ce689d4470ef1977863cd22d1b9c01f
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-01 01:02:48 -0600

    Actualizar diseño de cuadrícula en el componente DashboardNavbar

--- app/tablero/layout.js
+++ app/tablero/layout.js
@@ -8,9 +8,9 @@ const LayoutDashboard = async ({ children }) => {
   const session = await getServerSession(authOptions);
   if (session) {
     return (
-      <div className="flex">
+      <div className="flex min-h-screen">
         <DasboardSidebar />
-        <div className="flex-1 flex flex-col min-h-screen">
+        <div className="flex-1 flex flex-col">
           <DashboardNabvar className="sticky top-0 w-full z-50" />
           <main className="flex-1">
             {children}
@@ -19,11 +19,10 @@ const LayoutDashboard = async ({ children }) => {
       </div>
     );
   }
-  // return null;
   return (
-    <div className="flex">
+    <div className="flex min-h-screen">
       <DasboardSidebar />
-      <div className="flex-1 flex flex-col min-h-screen">
+      <div className="flex-1 flex flex-col">
         <DashboardNabvar className="sticky top-0 w-full z-50" />
         <main className="flex-1">
           {children}
--- components/component/dasboard-sidebar.jsx
+++ components/component/dasboard-sidebar.jsx
@@ -19,7 +19,7 @@ export default function DashboardSidebar() {
   return (
     <div className="md:relative md:w-64">
       {/* Sidebar for desktop */}
-      <div className={`fixed inset-y-0 left-0 z-50 flex flex-col h-screen max-w-[280px] dark:bg-zinc-900 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 md:relative`}>
+      <div className={`fixed inset-y-0 left-0 z-50 flex flex-col min-h-screen max-w-[280px] dark:bg-zinc-900 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 md:relative`}>
         <div className="flex items-center justify-between h-16 px-4 border-b border-zinc-200 dark:border-zinc-800">
           <Link className="ml-3 flex items-center gap-2 font-semibold text-lg md:text-xl" href="/">
             <MountainIcon className="h-8 w-8 md:h-10 md:w-10" />
