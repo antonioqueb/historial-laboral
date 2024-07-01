5f091828330394d87516d434bf8d587f9f61d5f5
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-01 03:02:27 -0600

    Actualizar diseño de cuadrícula en el componente DashboardSidebar

--- app/tablero/layout.js
+++ app/tablero/layout.js
@@ -10,9 +10,9 @@ const LayoutDashboard = async ({ children }) => {
     return (
       <div className="flex min-h-screen">
         <DasboardSidebar />
-        <div className="flex-1 flex flex-col min-h-screen">
+        <div className="flex-1 flex flex-col">
           <DashboardNabvar className="sticky top-0 w-full z-50" />
-          <main className="flex-1">
+          <main className="flex-1 overflow-y-auto">
             {children}
           </main>
         </div>
@@ -22,9 +22,9 @@ const LayoutDashboard = async ({ children }) => {
   return (
     <div className="flex min-h-screen">
       <DasboardSidebar />
-      <div className="flex-1 flex flex-col min-h-screen">
+      <div className="flex-1 flex flex-col">
         <DashboardNabvar className="sticky top-0 w-full z-50" />
-        <main className="flex-1">
+        <main className="flex-1 overflow-y-auto">
           {children}
         </main>
       </div>
