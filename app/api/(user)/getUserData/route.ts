9fcd73ca6c25f3c4040693eb2943ed473807beb9
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-04 12:53:01 -0600

    Actualizar manejo de errores en la obtención de la URL de la imagen de perfil

---
+++ app/api/(user)/getUserData/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const userId = searchParams.get('id');
+
+  if (!userId) {
+    return NextResponse.json({ message: 'User ID is required' }, { status: 400 });
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { email: true, name: true, profileImageUrl: true }, // Asegúrate de seleccionar los campos que necesitas
+    });
+
+    if (!user) {
+      return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(user, { status: 200 });
+  } catch (error: any) {
+    console.error('Error fetching user data:', error);
+    return NextResponse.json({ message: 'Error fetching user data', error: error.message }, { status: 500 });
+  }
+}
--- app/api/(user)/updateUserData/route.ts
+++ app/api/(user)/updateUserData/route.ts
@@ -1,3 +1,4 @@
+// app\api\(user)\updateUserData\route.ts
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
