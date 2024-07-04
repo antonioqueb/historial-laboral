392535b7a2ad57a7e0e8c4036e57d34c67c6f57e
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-04 01:36:26 -0600

    Actualizar componente de perfil para mostrar "Perfil" en lugar de "Profile"

--- app/tablero/perfil/page.tsx
+++ app/tablero/perfil/page.tsx
@@ -1,4 +1,47 @@
-// 
+// 'use client';
+// import React, { useEffect, useState } from 'react';
+// import ProfileImageUploader from '@/components/component/ProfileImageUploader';
+
+// const ProfilePage = () => {
+//   const [userId, setUserId] = useState<string | null>(null);
+//   const [error, setError] = useState<string | null>(null);
+
+//   useEffect(() => {
+//     const fetchUserId = async () => {
+//       try {
+//         const res = await fetch('https://historiallaboral.com/api/getUserId');
+//         if (res.ok) {
+//           const data = await res.json();
+//           setUserId(data.id);
+//         } else {
+//           setError('Failed to fetch user ID');
+//         }
+//       } catch (err) {
+//         setError('Failed to fetch user ID');
+//       }
+//     };
+
+//     fetchUserId();
+//   }, []);
+
+//   if (error) {
+//     return <div>Error: {error}</div>;
+//   }
+
+//   if (!userId) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div>
+//       <h1>Profile</h1>
+//       <ProfileImageUploader userId={userId} />
+//     </div>
+//   );
+// };
+
+// export default ProfilePage;
+
 
 
 import React from 'react'
