e3682660342f4da9dc76dd627228650963800113
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-04 02:07:12 -0600

    feat: Actualizar ruta de actualización de imagen de perfil de usuario

--- app/api/(user)/updateProfileImage/route.ts
+++ app/api/(user)/updateProfileImage/route.ts
@@ -4,21 +4,26 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
-  const { userId, imageUrl } = await request.json();
-
-  if (!userId || !imageUrl) {
-    return NextResponse.json({ message: 'User ID and Image URL are required' }, { status: 400 });
-  }
-
   try {
-    const user = await prisma.user.update({
-      where: { id: userId },
-      data: { profileImageUrl: imageUrl },
-    });
+    const { userId, imageUrl } = await request.json();
 
-    return NextResponse.json({ message: 'Profile image URL updated successfully', user });
-  } catch (error) {
-    console.error('Error updating profile image URL', error);
-    return NextResponse.json({ message: 'Error updating profile image URL' }, { status: 500 });
+    if (!userId || !imageUrl) {
+      return NextResponse.json({ message: 'User ID and Image URL are required' }, { status: 400 });
+    }
+
+    try {
+      const user = await prisma.user.update({
+        where: { id: userId },
+        data: { profileImageUrl: imageUrl },
+      });
+
+      return NextResponse.json({ message: 'Profile image URL updated successfully', user });
+    } catch (prismaError: any) {
+      console.error('Error updating profile image URL in Prisma', prismaError);
+      return NextResponse.json({ message: 'Error updating profile image URL in Prisma', error: prismaError.message }, { status: 500 });
+    }
+  } catch (jsonError: any) {
+    console.error('Error parsing JSON', jsonError);
+    return NextResponse.json({ message: 'Error parsing JSON', error: jsonError.message }, { status: 400 });
   }
 }
