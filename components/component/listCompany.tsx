c087dc83a848c5b874cac50dae14aa43cae74a66
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-07 17:47:46 -0600

    Actualizar vista de lista de empresas en ListCompany.tsx

--- components/component/listCompany.tsx
+++ components/component/listCompany.tsx
@@ -44,7 +44,7 @@ export default function ListCompany() {
       )}
       {session && (
         <div className="mb-20">
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between items-center mb-4">
             <h1 className="text-2xl font-bold text-zinc-900 dark:text-zinc-100">
               {companies.length > 1 ? "Empresas" : "Empresa"}
             </h1>
