154ebb75c209f84d32c72c8b9219e341f7b16fa0
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-11 15:14:23 -0600

    feat: Update company profile page with TypeScript implementation

--- app/perfil-empresarial/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { HeaderCompanyProfile } from '@/components/component/header-company-profile'
-
-export const Perfil = () => {
-  return (
-    <HeaderCompanyProfile />
-  )
-}
---
+++ app/perfil-empresarial/page.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { HeaderCompanyProfile } from '@/components/component/header-company-profile';
+
+// Componente principal de la página
+const Perfil: FC = () => {
+  return (
+    <HeaderCompanyProfile />
+  );
+}
+
+// Exportación por defecto del componente principal de la página
+export default Perfil;
