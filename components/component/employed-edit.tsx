1b5dd90cf95bdd0be768c6390b4318bbf40e8b06
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-20 19:32:40 -0600

    Actualizar URL de API en componente de edición de empleados

--- components/component/employed-edit.tsx
+++ components/component/employed-edit.tsx
@@ -277,7 +277,7 @@ export default function DashboardEmployedEdit() {
 
   const loadGenders = async () => {
     try {
-      const response = await fetch('/api/genders');
+      const response = await fetch('/api/Genders');
       const data = await response.json();
       setGenders(data.genders);
     } catch (error) {
