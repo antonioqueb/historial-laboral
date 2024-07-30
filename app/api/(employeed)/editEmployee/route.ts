556881cb5716f82db65848d0d960addff9e7b570
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-30 14:30:12 -0600

    Up

--- app/api/(employeed)/editEmployee/route.ts
+++ app/api/(employeed)/editEmployee/route.ts
@@ -3,7 +3,6 @@ import { authOptions } from "@/app/api/(auth)/auth/[...nextauth]/authOptions";
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
-// Define the type for the session with the user ID
 interface ExtendedSession {
   user: {
     id: string;
@@ -12,42 +11,6 @@ interface ExtendedSession {
     image?: string | null;
   };
 }
-
-export interface Employee {
-  id: string;
-  name: string;
-  role: string;
-  department: string;
-  companyId: string;
-  socialSecurityNumber: string;
-  CURP: string;
-  RFC: string;
-  address: string;
-  phoneNumber: string;
-  email: string;
-  birthDate: string;
-  hireDate: string;
-  emergencyContact: string;
-  emergencyPhone: string;
-  maritalStatus: string;
-  nationality: string;
-  educationLevel: string;
-  gender: string;
-  bloodType: string;
-  jobTitle: {
-    id: string;
-    name: string;
-  };
-  workShift: {
-    id: string;
-    name: string;
-  };
-  contractType: {
-    id: string;
-    name: string;
-  };
-}
-
 
 const prisma = new PrismaClient();
 
@@ -83,24 +46,20 @@ export async function PATCH(req: Request) {
 
       const uploadResult = await response.json();
       imageUrl = uploadResult.imageUrl;
-      // console.log("Image URL from upload:", imageUrl);
     }
 
     const updateData: Record<string, any> = Object.fromEntries(formData.entries());
-    // console.log("Initial updateData:", updateData);
     
-    delete updateData.id; // Remove id from update data as it should not be updated
+    delete updateData.id;
 
     if (imageUrl) {
       updateData.profileImageUrl = imageUrl;
     }
 
-    // Remove profileImage if it is empty or invalid
     if (!updateData.profileImage || Object.keys(updateData.profileImage).length === 0) {
       delete updateData.profileImage;
     }
 
-    // Convertir fechas a ISO 8601 asegurando que son strings
     if (typeof updateData.birthDate === 'string' && updateData.birthDate) {
       updateData.birthDate = new Date(updateData.birthDate).toISOString();
     }
@@ -108,19 +67,21 @@ export async function PATCH(req: Request) {
       updateData.hireDate = new Date(updateData.hireDate).toISOString();
     }
 
-    // console.log("Processed updateData before deleting unwanted fields:", updateData);
-
-    // Eliminar campos que no deberían ser actualizados
     delete updateData.company;
-    delete updateData.description; // Eliminar description del objeto de actualización
-    delete updateData.bankAccountNumber; // Eliminar bankAccountNumber del objeto de actualización
-    delete updateData.clabeNumber; // Eliminar clabeNumber del objeto de actualización
-
-    // console.log("Final updateData to be sent to Prisma:", updateData);
+    delete updateData.description;
+    delete updateData.bankAccountNumber;
+    delete updateData.clabeNumber;
 
     const updatedEmployee = await prisma.employee.update({
       where: { id: id as string },
-      data: updateData,
+      data: {
+        ...updateData,
+        jobTitle: { connect: { id: updateData.jobTitle } },
+        workShift: { connect: { id: updateData.workShift } },
+        contractType: { connect: { id: updateData.contractType } },
+        role: { connect: { id: updateData.role } },
+        department: { connect: { id: updateData.department } },
+      },
     });
 
     console.log("Updated employee:", updatedEmployee);
