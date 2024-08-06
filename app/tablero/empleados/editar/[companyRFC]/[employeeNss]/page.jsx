fed44e412e709879e0523dbc15fae412bfdc7041
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-05 18:42:26 -0600

    Up

---
+++ app/tablero/empleados/editar/[companyRFC]/[employeeNss]/page.jsx
@@ -0,0 +1,8 @@
+// app/tablero/empleados/editar/[companyRFC]/[employeeNss]/page.jsx
+import DashboardEmployedEdit from '@/components/component/employed-edit';
+
+const EditEmployedPage = () => {
+  return <DashboardEmployedEdit />;
+};
+
+export default EditEmployedPage;
