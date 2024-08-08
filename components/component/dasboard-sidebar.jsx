cb8caaed1c72de9606bd96884b058be0c2969c89
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-07 22:20:22 -0600

    Up

--- components/component/dasboard-sidebar.jsx
+++ components/component/dasboard-sidebar.jsx
@@ -28,8 +28,8 @@ export default function DashboardSidebar() {
           <Image
           src="/logo-full.svg"
           alt="Logo"
-          width={60}
-          height={60}
+          width={120}
+          height={120}
         />
           </Link>
           <button className="md:hidden" onClick={toggleSidebar}>
